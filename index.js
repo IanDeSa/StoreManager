dc09d6259d853aa33f0564156818c2e07da464a7
Ian Santos <contato.iansantos@gmail.com>
2022-08-16 01:58:10 -0400

    feat: criação de end point para /products criada e associada as rotas de products

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+const express = require('express');
 const app = require('./app');
+const routes = require('./routes');
+
+app.use(express.json());
 require('dotenv').config();
+
+app.use('/products', routes.productsRoute);
 
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
 
