396d8e6798ea754b8f139e1a24f2fda6c88cb68f
Ian Santos <contato.iansantos@gmail.com>
2022-08-16 01:55:34 -0400

    feat: criação de duas funções que fazem a chamada ao banco de dados, getAll para buscar todos os dados e getById para buscar onde o Id passado seja igual ao da requisição

---
+++ models/productsModel.js
@@ -0,0 +1,16 @@
+const connection = require('./connection');
+
+const getAll = async () => { 
+  const [result] = await connection.execute('SELECT * FROM products;');
+  return result;
+};
+
+const getById = async (id) => {
+  const [result] = await connection.execute('SELECT * FROM products WHERE id = ?;', [id]);
+  return result;
+};
+
+module.exports = {
+  getAll,
+  getById,
+};
