79caef4c313aec7b27f7e5dc61e8f8ea9182193f
Ian Santos <contato.iansantos@gmail.com>
2022-08-17 00:52:00 -0400

    feat: criação de serviço para o controller se comunicar com o model de addProduct

--- services/productsService.js
+++ services/productsService.js
@@ -4,7 +4,10 @@ const getAll = async () => productsModel.getAll();
 
 const getById = async (id) => productsModel.getById(id);
 
+const addProduct = async (name) => productsModel.addProduct(name);
+
 module.exports = {
   getAll,
   getById,
+  addProduct,
 };
