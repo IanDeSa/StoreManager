dcd2573e9769ac6adf85704f2dceb30d1fa57d63
Ian Santos <contato.iansantos@gmail.com>
2022-08-17 02:58:14 -0400

    feat: adição de try/catch para capturar erros internos

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,22 +1,36 @@
 const productsService = require('../services/productsService');
 
-const getAll = async (_req, res) => {
-  const result = await productsService.getAll();
-  if (!result || result.length === 0) return res.status(404).json({ message: 'Product not found' });
-  return res.status(200).json(result);
+const message = { message: 'Product not found' };
+
+const getAll = async (_req, res, next) => {
+  try {
+    const result = await productsService.getAll();
+    // if (!result || result.length === 0) return res.status(404).json(message);
+    return res.status(200).json(result);
+  } catch (err) {
+    next(err);
+  }
 };
 
-const getById = async (req, res) => {
-  const { id } = req.params;
-  const [result] = await productsService.getById(id);
-  if (!result || result.length === 0) return res.status(404).json({ message: 'Product not found' });
-  return res.status(200).json(result);
+const getById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const [result] = await productsService.getById(id);
+    if (!result || result.length === 0) return res.status(404).json(message);
+    return res.status(200).json(result);
+  } catch (err) {
+    next(err);
+  }
 };
 
-const addProduct = async (req, res) => {
-  const { name } = req.body;
-  const response = await productsService.addProduct(name);
-  return res.status(201).json(response);
+const addProduct = async (req, res, next) => {
+  try {
+    const { name } = req.body;
+    const response = await productsService.addProduct(name);
+    return res.status(201).json(response);
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports = {
