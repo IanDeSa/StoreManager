3c13436463fbf70af1b03989520fe6d5ce57b19f
Ian Santos <contato.iansantos@gmail.com>
2022-08-18 00:20:59 -0400

    feat: criação de controller getAll e getById

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -3,11 +3,31 @@ const salesService = require('../services/salesService');
 const addSale = async (req, res, next) => {
   try {
     const sales = req.body;
-    const response = await salesService.addSale(sales);
-    return res.status(201).json(response);
+    const result = await salesService.addSale(sales);
+    return res.status(201).json(result);
   } catch (error) {
     next(error);
   }
 };
 
-module.exports = { addSale };
+const getAll = async (_req, res, next) => {
+  try {
+    const result = await salesService.getAll();
+    return res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await salesService.getById(id);
+    if (result.length === 0) res.status(404).json({ message: 'Sale not found' });
+    return res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { addSale, getAll, getById };
