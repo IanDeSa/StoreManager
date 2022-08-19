0058eb75ba418051b26daf487aab210b656a049c
Ian Santos <contato.iansantos@gmail.com>
2022-08-18 22:31:23 -0400

    feat: adiciona a função addSale a camada sales controller

--- controllers/salesController.js
+++ controllers/salesController.js
@@ -1,18 +1,9 @@
 const salesService = require('../services/salesService');
 
-const addSale = async (req, res, next) => {
-  try {
-    const sales = req.body;
-    const result = await salesService.addSale(sales);
-    return res.status(201).json(result);
-  } catch (error) {
-    next(error);
-  }
-};
-
 const getAll = async (_req, res, next) => {
   try {
     const result = await salesService.getAll();
+    if (!result) return res.status(404).json({ message: 'Sale not found' });
     return res.status(200).json(result);
   } catch (error) {
     next(error);
@@ -23,8 +14,19 @@ const getById = async (req, res, next) => {
   try {
     const { id } = req.params;
     const result = await salesService.getById(id);
-    if (result.length === 0) res.status(404).json({ message: 'Sale not found' });
+    if (!result) return res.status(404).json({ message: 'Sale not found' });
     return res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const addSale = async (req, res, next) => {
+  try {
+    const sales = req.body;
+    const result = await salesService.addSale(sales);
+    if (!result) return res.status(404).json({ message: 'Product not found' });
+    return res.status(201).json(result);
   } catch (error) {
     next(error);
   }
