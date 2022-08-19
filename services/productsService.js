1c1da953d311818391cdce88647792bd4af6b3fd
Ian Santos <contato.iansantos@gmail.com>
2022-08-18 22:22:09 -0400

    feat: adiciona duas funções a camada de service, add e destroy

--- services/productsService.js
+++ services/productsService.js
@@ -2,12 +2,30 @@ const productsModel = require('../models/productsModel');
 
 const getAll = async () => productsModel.getAll();
 
-const getById = async (id) => productsModel.getById(id);
+const getById = async (id) => {
+  const [result] = await productsModel.getById(id);
+  if (!result) return null;
+  return result;
+};
 
 const addProduct = async (name) => productsModel.addProduct(name);
+
+const update = async ({ name, id }) => {
+  const result = await productsModel.update({ name, id });
+  if (!result) return false;
+  return result;
+};
+
+const destroy = async (id) => {
+  const validation = await productsModel.getById(id);
+  if (!validation || validation.message) return null;
+  return productsModel.destroy(id);
+};
 
 module.exports = {
   getAll,
   getById,
   addProduct,
+  update,
+  destroy,
 };
