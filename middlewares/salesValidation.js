f4d646c0946c49b1b1c874055830e43145c5e16b
Ian Santos <contato.iansantos@gmail.com>
2022-08-18 22:32:06 -0400

    feat: adiciona validação para a rota sales

--- middlewares/salesValidation.js
+++ middlewares/salesValidation.js
@@ -1,16 +1,16 @@
 const validationSales = {
   isRequired: async (req, res, next) => {
     const sales = req.body;
-    const products = sales.filter((sale) => sale.productId);
-    const quantitys = sales.filter((sale) => sale.quantity);
+    const products = sales.every((sale) => Object.keys(sale).includes('productId'));
+    const quantitys = sales.every((sale) => Object.keys(sale).includes('quantity'));
     const valueZero = sales.some((sale) => sale.quantity <= 0);
 
     switch (true) {
-      case (!products.length):
-        return res.status(400).json({ message: '"productId" is required' }); 
+      case (!products):
+        return res.status(400).json({ message: '"productId" is required' });
       case (valueZero):
         return res.status(422).json({ message: '"quantity" must be greater than or equal to 1' });
-      case (!quantitys.length):
+      case (!quantitys):
         return res.status(400).json({ message: '"quantity" is required' });
       default:
         break;
