bec4c5f588457002239a129c90320bb778c98ac0
Ian Santos <contato.iansantos@gmail.com>
2022-08-18 22:20:37 -0400

    feat: adiciona a função de add a camada service

--- services/salesService.js
+++ services/salesService.js
@@ -1,9 +1,27 @@
 const salesModel = require('../models/salesModel');
+const productsService = require('./productsService');
 
-const addSale = async (sales) => salesModel.addSale(sales);
+const getById = async (id) => {
+  const result = await salesModel.getById(id);
+  if (!result || result.length === 0) return null;
+  return result;
+};
 
-const getAll = async () => salesModel.getAll();
+const getAll = async () => {
+  const result = await salesModel.getAll();
+  if (!result) return null;
+  return result;
+};
 
-const getById = async (id) => salesModel.getById(id);
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+const addSale = async (sales) => {
+  const notFound = await sales.reduce(async (acc, cur) => {
+    const validateById = await productsService.getById(cur.productId);
+    if (!validateById) return true;
+    return acc;
+  }, false);
+  if (notFound) return null;
+  return salesModel.addSale(sales);
+};
 
 module.exports = { addSale, getAll, getById };
